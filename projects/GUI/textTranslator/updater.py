38887b36f2441fd04715ac01ef1f6018b2371644
Abdul Zoha <abdulzoha786@gmail.com>
2023-05-05 23:04:34 +0300

    On branch master Changes to be committed: 	modified:   projects/GUI/textTranslator/updater.py

--- projects/GUI/textTranslator/updater.py
+++ projects/GUI/textTranslator/updater.py
@@ -1,6 +1,6 @@
 import db
 
-def trans(arg):
+def trans():
     clear_notFound()
     clear_database()
     """filtering Data"""
@@ -15,7 +15,6 @@ def trans(arg):
              insert_eng(miss)
              continue
     populate()
-    total()
     
 def update(arg):
     clear_database()
@@ -27,7 +26,7 @@ def update(arg):
         if english:
             if hindi:
                 db.insert(english, hindi)
-    trans(arg)
+    trans()
 
 def populate():
     n = 0
@@ -35,7 +34,7 @@ def populate():
     for record in data:
         eng, hin = record
         n += 1
-        record = f"{n} => {eng} => {hin}"
+        record = f"{n}:   {eng}   =>   {hin}"
         database.insert(END, record)
 
 def total():
@@ -63,45 +62,32 @@ root.config(bg="green")
 lbl_title = Label(root, text="L2S Translator")
 lbl_title.pack(pady=10)
 
-"""Button"""
-update_btn = Button(root, text="Home")
-update_btn.place(relx=0.02, rely=0.04)
-
 """Labels"""
 lbl_total = Label(root, text="Total => ")
-lbl_total.place(relx=0.84, rely=0.04)
+lbl_total.place(relx=0.92, rely=0.01)
 
-lbl_eng = Label(root, text="Eng:")
-lbl_eng.place(relx=0.02, rely=0.10)
+lbl_eng = Label(root, text="Field1:")
+lbl_eng.place(relx=0.02, rely=0.07)
 
-lbl_hin = Label(root, text="Hin:")
-lbl_hin.place(relx=0.02, rely=0.16)
-
-lbl_source = Label(root, text="Source")
-lbl_source.place(relx=0.24, rely=0.21)
-
-lbl_notFound = Label(root, text="Not Found")
-lbl_notFound.place(relx=0.72, rely=0.21)
-
-lbl_database = Label(root, text="Database")
-lbl_database.place(relx=0.48, rely=0.61)
+lbl_hin = Label(root, text="Field2:")
+lbl_hin.place(relx=0.02, rely=0.12)
 
 """Entry"""
 ent_eng = Entry(root)
-ent_eng.place(relx=0.05, rely=0.10, relwidth=0.93)
+ent_eng.place(relx=0.08, rely=0.07, relwidth=0.90)
 
 ent_hin = Entry(root)
-ent_hin.place(relx=0.05, rely=0.16, relwidth=0.93)
+ent_hin.place(relx=0.08, rely=0.12, relwidth=0.90)
 
 """Text"""
 source = Text(root, padx=10, pady=10)
-source.place(relx=0.02, rely=0.25, relwidth=0.48, relheight=0.35)
+source.place(relx=0.02, rely=0.17, relwidth=0.96, relheight=0.38)
 
 notFound = Text(root, padx=10, pady=10)
-notFound.place(relx=0.50, rely=0.25, relwidth=0.48, relheight=0.35)
+notFound.place(relx=0.02, rely=0.56, relwidth=0.49, relheight=0.37)
 
 frm_lbx = Frame(root)
-frm_lbx.place(relx=0.02, rely=0.64, relwidth=0.96, relheight=0.35)
+frm_lbx.place(relx=0.52, rely=0.56, relwidth=0.46, relheight=0.37)
 
 """List box"""
 database = Listbox(frm_lbx)
@@ -112,11 +98,28 @@ scrollbar.pack(side=RIGHT, fill=Y)
 database.config(yscrollcommand=scrollbar.set) #to configure listbox
 scrollbar.config(command=database.yview) #to configure scrollbar
 
+"""Frame"""
+btn_frm = Frame(root)
+btn_frm.place(relx=0.3, rely=0.94, relwidth=0.45, relheight=0.05)
+
+"""Button"""
+add_btn = Button(btn_frm, text="Check", command=trans)
+add_btn.place(relwidth=0.25, relheight=1)
+
+update_btn = Button(btn_frm, text="Add")
+update_btn.place(relx=0.25, relwidth=0.25, relheight=1)
+
+update_btn = Button(btn_frm, text="Update")
+update_btn.place(relx=0.50, relwidth=0.25, relheight=1)
+
+update_btn = Button(btn_frm, text="Delete")
+update_btn.place(relx=0.75, relwidth=0.25, relheight=1)
+
 """calling functions"""
 populate()
 total()
 
-"""keybind"""
-root.bind("<Control_L><s>", update)
-root.bind("<Control_L><t>", trans)
+# """keybind"""
+# root.bind("<Control_L><s>", update)
+# root.bind("<Control_L><t>", trans)
 root.mainloop()
