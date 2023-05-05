ac5e4907661d6ee951dd030f88bcac140f2f4c01
Abdul Zoha <abdulzoha786@gmail.com>
2023-05-05 09:11:31 +0300

    On branch master Changes to be committed: 	modified:   .gitignore 	renamed:    projects/GUI/textTranslator 0.3/db.py -> projects/GUI/textTranslator/db.py 	renamed:    projects/GUI/textTranslator 0.3/source.txt -> projects/GUI/textTranslator/source.txt 	renamed:    projects/GUI/textTranslator 0.3/sqlite.db -> projects/GUI/textTranslator/sqlite.db 	renamed:    projects/GUI/textTranslator 0.3/strftr.py -> projects/GUI/textTranslator/strftr.py 	renamed:    projects/GUI/textTranslator 0.3/translator.py -> projects/GUI/textTranslator/translator.py 	renamed:    projects/GUI/textTranslator 0.3/updater.py -> projects/GUI/textTranslator/updater.py 	modified:   projects/GUI/translate/edit.py

--- projects/GUI/translate/edit.py
+++ projects/GUI/translate/edit.py
@@ -12,7 +12,6 @@ def update():
     input1.delete(0, END)
     input2.delete(0, END)
     populate()
-    populate2()
 def add(event):
     inp1 = input1.get()
     inp2 = input2.get()
@@ -23,7 +22,6 @@ def add(event):
         input1.delete(0, END)
         input2.delete(0, END)
         populate()
-        populate2()
         records()
 def delete():
     inp1 = input1.get()
@@ -35,23 +33,21 @@ def delete():
         input1.delete(0, END)
         input2.delete(0, END)
         populate()
-        populate2()
         records()
 def populate():
     lbx.delete(0, END)
-    for rows in dbhelper.show():
-        lbx.insert(END, rows[1])
-def populate2():
-    lbx2.delete(0, END)
-    for rows in dbhelper.show():
-        lbx2.insert(END, rows[2])
+    for index, rows in enumerate(dbhelper.show()):
+        english = rows[1]
+        hindi = rows[2]
+        data = f"{index}: {english}    =>    {hindi}"
+        lbx.insert(END, data)
 def records():
     cnt = f"Total: {dbhelper.count1()}"
     total.config(text=cnt)
 
 root = Tk()
 root.title("Translator")
-root.geometry("1620x500+1820+0")
+# root.geometry("1620x500+1820+0")
 root.config(bg="maroon")
 root.resizable(0,0)
 pt = PhotoImage(file = 'icon.png')
@@ -60,59 +56,40 @@ root.iconphoto(False, pt)
 main = Frame(root, bg="maroon")
 main.pack()
 
-Label(main, text="ARTIFICIALWORLD01", font=("Arial",30, "bold")).grid(row=0, column=0, pady=(20, 10), columnspan=2)
+Label(main, text="L2S Translator", font=("Arial",30, "bold")).grid(row=1, column=0, pady=(20, 10), columnspan=2)
 
 total = Label(main, text="Total:", padx=10)
-total.grid(row=1, column=0, columnspan=2)
+total.grid(row=0, column=1, columnspan=2, sticky=E)
 records()
 
-Label(main, text="TRANSLATE", padx=10, bg="maroon", fg="white").grid(row=2, column=0)
+Label(main, text="Eng:", padx=10, bg="maroon", fg="white").grid(row=2, column=0, sticky=W)
 
 input1 = Entry(main, font=("arial", 16, "bold"))
-input1.grid(row=3, column=0, ipadx=260)
+input1.grid(row=2, column=1, ipadx=160, sticky=W)
 
-Label(main, text="TRANSLATED", padx=10, bg="maroon", fg="white").grid(row=2, column=1)
+Label(main, text="Hin:", padx=10, bg="maroon", fg="white").grid(row=3, column=0, sticky=W)
 
 input2 = Entry(main, font=("arial", 16, "bold"))
-input2.grid(row=3, column=1, ipadx=260)
+input2.grid(row=3, column=1, ipadx=160, sticky=W)
 
-Label(main, text="LIST-1", padx=10, bg="maroon", fg="white").grid(row=5, column=0, pady=(10, 0))
+Label(main, text="Word Lists", padx=10, bg="maroon", fg="white").grid(row=5, column=1, pady=(10, 0))
 
-Label(main, text="LIST-2", padx=10, bg="maroon", fg="white").grid(row=5, column=1, pady=(10, 0))
-
-"""defining a Frame for listbox-1"""
-frm_lbx1 = Frame(main,bd=2,relief=RIDGE)
-frm_lbx1.grid(row=6, column=0, ipadx=390, ipady=100, padx=(0,10))
+"""defining a Frame for listbox"""
+frm_lbx2 = Frame(main,bd=2, relief=RIDGE)
+frm_lbx2.grid(row=6, column=0, ipadx=300, ipady=100, columnspan=2)
 
 """defining a Listbox"""
-lbx = Listbox(frm_lbx1,justify=CENTER,font=("arial", 18, "bold"))
+lbx = Listbox(frm_lbx2,font=("arial", 18, "bold"))
 lbx.place(x=0, y=0, relwidth=1, relheight=1)
-
-"""defining a Scrollbar"""
-scroll = Scrollbar(frm_lbx1,orient=VERTICAL)
-scroll.pack(side=RIGHT,fill=Y)
-
-"""to configure scrollbar with listbox"""
-lbx.config(yscrollcommand=scroll.set)
-scroll.config(command=lbx.yview)
-populate()
-
-"""defining a Frame for listbox-2"""
-frm_lbx2 = Frame(main,bd=2,relief=RIDGE)
-frm_lbx2.grid(row=6, column=1, ipadx=390, ipady=100)
-
-"""defining a Listbox"""
-lbx2 = Listbox(frm_lbx2,justify=CENTER,font=("arial", 18, "bold"))
-lbx2.place(x=0, y=0, relwidth=1, relheight=1)
 
 """defining a Scrollbar"""
 scroll2 = Scrollbar(frm_lbx2,orient=VERTICAL)
 scroll2.pack(side=RIGHT,fill=Y)
 
 """to configure scrollbar with listbox"""
-lbx2.config(yscrollcommand=scroll2.set)
-scroll2.config(command=lbx2.yview)
-populate2()
+lbx.config(yscrollcommand=scroll2.set)
+scroll2.config(command=lbx.yview)
+populate()
 
 updated = Label(main, text="Last update: 24/03/2022", bg="maroon", fg="white")
 updated.grid(row=7, column=0, columnspan=2, pady=(10,0))
