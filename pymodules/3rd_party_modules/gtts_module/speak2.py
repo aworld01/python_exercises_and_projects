5729bc996f06bdaedbde9ef7c27f4c4ccad12b2c
Abdul Zoha <abdulzoha786@gmail.com>
2023-05-10 22:21:23 +0300

    On branch master Changes to be committed: 	modified:   projects/GUI/translate/dictionary.db 	new file:   projects/GUI/translate/meanings.txt 	modified:   pymodules/3rd_party_modules/gtts_module/speak.py 	new file:   pymodules/3rd_party_modules/gtts_module/speak2.py

--- pymodules/3rd_party_modules/gtts_module/speak2.py
+++ pymodules/3rd_party_modules/gtts_module/speak2.py
@@ -2,31 +2,31 @@ import os
 from gtts import gTTS #pip install gtts
 from playsound import playsound #pip install playsound
 
-def speak(txt, fileName="default"):
+def fileSave(txt, fileName="default"):
     tts = gTTS(text=txt, lang="hi") #creating instance
-
     """make directory if not exist"""
     if not os.path.exists("audios"):
         os.mkdir("audios")
-
     """file naming"""
     if fileName == "default":
         fileName = f"audios/{txt}.mp3".lower()
     else:
         fileName = f"audios/{fileName}.mp3".lower()
-
     """checking if fileName is not exists then save and play"""
-    if not os.path.exists(fileName):
-        tts.save(fileName)
+    try:
+        if not os.path.exists(fileName):
+            tts.save(fileName)
+    except:
+        playsound("The file your are trying to play is not exist")
+def filePlay(data):
+    fileName = f"audios/{data}.mp3".lower()
+    if os.path.exists(fileName):
         playsound(fileName)
-    else:
-        playsound(fileName) #play if fileName is exists
        
 if(__name__=='__main__'):
    strEng = "HEllo world how are you"
    strHin = "नमस्ते दुनिया वालों आप कैसे हो"
+   string = "The file your are trying to play is not exist"
 
-   speak(strEng) #example1 (strEng as a data)
-#    speak(strHin) #example2 (strHin as a data)
-#    speak(strHin,strEng) #example3 (strHin as a data, strEng as a fileName)
-#    speak(strEng,strHin) #example4 (strEng as a data, strHin as a fileName)
+   fileSave(string) #example1 (strEng as a data)
+   filePlay(strEng)
