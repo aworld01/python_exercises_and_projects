e9a49e56ebf28afc14c6971c1664a51d96bde58f
Abdul Zoha <abdulzoha786@gmail.com>
2023-09-12 04:33:11 +0300

    On branch master Changes to be committed: 	modified:   projects/GUI/wordGame/getData.py

--- projects/GUI/wordGame/getData.py
+++ projects/GUI/wordGame/getData.py
@@ -2,15 +2,32 @@ def newData(arg):
     db = {}
     with open(arg, mode="r", encoding="utf-8") as rf:
         data = rf.read()
-        data = data.split("\n")
-        for record in data:
-            record = record.replace(", ", ",")
-            eng, hin = record.split(": ")
-            db.update({eng: hin})
+        # print(data, type(data))
+
+    data = data.split("\n") #split data throw newline
+    # print(data, type(data))
+
+    data = [i.strip() for i in data] #remove whitespaces for both side of items
+    # print(data)
+
+    data = [i for i in data if i != ""] #to remove empty items
+    # print(data)
+
+    """split by ": " and add into dictionary"""
+    for i in data:
+        eng, hin = i.split(": ")
+        db.update({eng:hin})
+
     return db
+
+
+
+    
+
 
 if __name__ == "__main__":
     file = "data.txt"
 
     for key, value in newData(file).items():
-        print(f"{key} => {value}")
+        print(f"{key} => {value}check")
+    print("Done")
