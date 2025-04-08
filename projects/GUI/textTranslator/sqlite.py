fde2d8d90cd2efdd6eaca51dcab39924a20cd283
Windows 11 <abdulzoha786@gmail.com>
2025-04-08 07:31:34 +0530

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	deleted:    projects/GUI/textTranslator/dictionary.db
    	deleted:    projects/GUI/textTranslator/hin2eng.txt
    	modified:   projects/GUI/textTranslator/sqlite.py
    	modified:   projects/GUI/textTranslator/temp.txt

--- projects/GUI/textTranslator/sqlite.py
+++ projects/GUI/textTranslator/sqlite.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-con = sqlite3.connect("dictionary.db")
+con = sqlite3.connect("sqlite.db")
 
 def show():
-    tables = con.execute("SELECT * FROM dict")
+    tables = con.execute("SELECT * FROM tran")
     return tables
 
 data = ""
-for _,eng,hin in show():
+for eng,hin in show():
     data += f"{eng} => {hin}\n"
 
-with open('temp.txt',mode='w',encoding="utf-8") as rf:
+with open('temp2.txt',mode='w',encoding="utf-8") as rf:
     rf.write(data)
